6c50469378edcb6f0c2d4c1a5d7a4a71977b3ef5
Oleksandr Zinkevych <zso040399@gmail.com>
2024-07-20 18:46:53 +0300

    Add websocket client

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {writable} from "svelte/store";
+
+    const messages = writable<string[]>([]);
+
+    const url = "ws://localhost:8080/ws";
+
+    const ws = new WebSocket(url);
+    ws.onopen = () => {
+        console.log("Connected to the server");
+    };
+    ws.onmessage = (event) => {
+        console.log(event.data);
+        messages.update((messages) => [...messages, JSON.parse(event.data).message]);
+    };
+    
+
+let name: string | null = null;
+</script>
+
+
+<input bind:value={name} placeholder="Enter your name" />
+<!-- <button on:click={() => name ? (messages = [...messages, `Hello ${name}!`]) : null}> Say hello </button> -->
+
+{#each $messages as message, i (i)}
+	<p>{message}</p>
+{/each}
